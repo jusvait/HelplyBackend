f20141396abcf014b2f94f57fdfc1cd4d3368f31
HansenFeng <hansen.feng@smartly.io>
2022-11-05 21:30:30 +0200

    cors

--- src/main.rs
+++ src/main.rs
@@ -40,9 +40,34 @@ async fn create_note(ticket_id: i32, note: Json<NewNote>) -> Json<Note> {
     Json (note)
 }
 
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+
 #[launch]
 fn stage() -> _ {
     rocket::build()
+        .attach(CORS)
         .mount("/", routes![index])
         .mount("/ticket", routes![get_many, get_one, create, update, create_note])
 }
